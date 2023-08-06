b064f6d6c0885bc379d6eaf2da51e666cd8d8881
Fernanda Kipper <nanda.kipper@gmail.com>
2023-08-06 12:29:18 -0300

    fix: adjust sendgrid initialization

--- index.mjs
+++ index.mjs
@@ -1,5 +1,7 @@
 import sgMail from '@sendgrid/mail'
 
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
 export const handler = async(event) => {
     let body = JSON.parse(event.body)
     let email = body?.email
@@ -9,18 +11,14 @@ export const handler = async(event) => {
         body: 'Email was not provided',
     };
 
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-    console.log(process.env.SENDGRID_API_KEY)
-    console.log(process.env.SENDGRID_SENDER_EMAIL)
-
     const msg = {
         to: email,
-        from: process.env.SENDGRID_SENDER_EMAIL, // Mude para seu email verificado no sendgrid
+        from: process.env.SENDGRID_SENDER_EMAIL,
         subject: 'Sending with SendGrid is Fun',
         text: 'and easy to do anywhere, even with Node.js',
         html: '<strong>and easy to do anywhere, even with Node.js</strong>',
     }
+
     sgMail
     .send(msg)
     .then(() => {
